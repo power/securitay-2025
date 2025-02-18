9501a113bfb272b73817a4cacc0485579fcb7137
David <29968843+power@users.noreply.github.com>
2025-02-18 10:59:43 +0000

    Included multi-threading

--- RPSK/challenge.py
+++ RPSK/challenge.py
@@ -1,8 +1,7 @@
-# docker build -t rpsk .
-# docker run -d -p 38172:38172 --name rpsk rpsk
 import ctypes
 import random
 import socket
+import threading
 import time
 
 HOST = "0.0.0.0"
@@ -10,7 +9,7 @@ PORT = 38172
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
-server_socket.listen(5)  # Listen for up to 5 connections
+server_socket.listen(50)  # Listen for up to 5 connections
 print(f"Listening for connections on {HOST}:{PORT}...")
 
 class CTFChallenge:
@@ -19,22 +18,22 @@ class CTFChallenge:
         self.secret_value = ctypes.c_int(0)  # 32-bit signed integer
 
     def send(self, message):
-        """Send a message to the client."""
         try:
             self.client_socket.sendall(message.encode() + b"\n")
         except:
             self.client_socket.close()
+
     def receive(self):
-        """Receive input from the client."""
         try:
             return self.client_socket.recv(1024).decode().strip()
         except:
             self.client_socket.close()
+            return None
 
     def add_points(self, points):
-        self.secret_value.value += points 
-        if self.secret_value.value < -10000000:  # Overflowed into negative
-            self.send("SECURI-TAY{315cd75e7e1a5c34918b402851e3ebda}")
+        self.secret_value.value += points
+        if self.secret_value.value < -10000000:
+            self.send("I guess you win.. Take the flag and leave! SECURI-TAY{315cd75e7e1a5c34918b402851e3ebda}")
         else:
             self.send(f"Current points: {self.secret_value.value}")
 
@@ -48,7 +47,7 @@ Welcome to my Rock Paper Scissors Game.
 1. Play
 2. View Points
 3. Quit
-4. Developer Mode 
+4. Developer Mode
 5. Dev Mode (REMOVED)
 6. Super Duper Secret Dev Mode (NOT WORKING)
 Enter your choice:""")
@@ -65,76 +64,32 @@ Enter your choice:""")
         choice = self.receive().lower()
         comp_choice = random.choice(options)
 
-        if choice == "r":
-            if comp_choice == "r":
-                self.send("You both chose Rock! No points added.")
-            elif comp_choice == "p":
-                self.secret_value.value -= 3
-                self.send(f"Computer chose Paper, you lose 3 points! You now have: {self.secret_value.value} points")
-            elif comp_choice == "s":
-                self.secret_value.value += 3
-                self.send(f"Computer chose Scissors, you win 3 points! You now have: {self.secret_value.value} points")
-
-        elif choice == "p":
-            if comp_choice == "p":
-                self.send("You both chose Paper! No points added.")
-            elif comp_choice == "r":
-                self.secret_value.value += 3
-                self.send(f"Computer chose Rock, you win 3 points! You now have: {self.secret_value.value} points")
-            elif comp_choice == "s":
-                self.secret_value.value -= 3
-                self.send(f"Computer chose Scissors, you lose 3 points! You now have: {self.secret_value.value} points")
-
-        elif choice == "s":
-            if comp_choice == "s":
-                self.send("You both chose Scissors! No points added.")
-            elif comp_choice == "p":
-                self.secret_value.value += 3
-                self.send(f"Computer chose Paper, you win 3 points! You now have: {self.secret_value.value} points")
-            elif comp_choice == "r":
-                self.secret_value.value -= 3
-                self.send(f"Computer chose Rock, you lose 3 points! You now have: {self.secret_value.value} points")
+        if choice in options:
+            outcome = {
+                ("r", "s"): 3, ("r", "p"): -3, ("r", "r"): 0,
+                ("p", "r"): 3, ("p", "s"): -3, ("p", "p"): 0,
+                ("s", "p"): 3, ("s", "r"): -3, ("s", "s"): 0,
+            }
+            self.secret_value.value += outcome.get((choice, comp_choice), 0)
+            self.send(f"Computer chose {comp_choice.upper()}, you {'win' if outcome.get((choice, comp_choice), 0) > 0 else 'lose' if outcome.get((choice, comp_choice), 0) < 0 else 'draw'}! You now have: {self.secret_value.value} points")
         else:
             self.send("Invalid choice. Please enter R, P, or S.")
 
     def special_rps(self):
-        self.send(""">>> DEVELOPER MODE EXCLUSIVE <<<
-New version of Rock Paper Scissors that includes a new move! Doesn't handle integers nicely so make sure to input correctly!""")
+        self.send(">>> DEVELOPER MODE EXCLUSIVE <<<\nNew version of Rock Paper Scissors that includes a new move! Doesn't handle integers nicely so make sure to input correctly!")
         options = ["r", "p", "s"]
         self.send("Please choose: Rock (R), Paper (P), Scissors (S) or Karate Chop (K)")
         choice = self.receive().lower()
         comp_choice = random.choice(options)
 
-        if choice == "r":
-            if comp_choice == "r":
-                self.send("You both chose Rock! No points added.")
-            elif comp_choice == "p":
-                self.secret_value.value -= 3
-                self.send(f"Computer chose Paper, you lose 3 points! You now have: {self.secret_value.value} points")
-            elif comp_choice == "s":
-                self.secret_value.value += 3
-                self.send(f"Computer chose Scissors, you win 3 points! You now have: {self.secret_value.value} points")
-
-        elif choice == "p":
-            if comp_choice == "p":
-                self.send("You both chose Paper! No points added.")
-            elif comp_choice == "r":
-                self.secret_value.value += 3
-                self.send(f"Computer chose Rock, you win 3 points! You now have: {self.secret_value.value} points")
-            elif comp_choice == "s":
-                self.secret_value.value -= 3
-                self.send(f"Computer chose Scissors, you lose 3 points! You now have: {self.secret_value.value} points")
-
-        elif choice == "s":
-            if comp_choice == "s":
-                self.send("You both chose Scissors! No points added.")
-            elif comp_choice == "p":
-                self.secret_value.value += 3
-                self.send(f"Computer chose Paper, you win 3 points! You now have: {self.secret_value.value} points")
-            elif comp_choice == "r":
-                self.secret_value.value -= 3
-                self.send(f"Computer chose Rock, you lose 3 points! You now have: {self.secret_value.value} points")
-
+        if choice in options:
+            outcome = {
+                ("r", "s"): 3, ("r", "p"): -3, ("r", "r"): 0,
+                ("p", "r"): 3, ("p", "s"): -3, ("p", "p"): 0,
+                ("s", "p"): 3, ("s", "r"): -3, ("s", "s"): 0,
+            }
+            self.secret_value.value += outcome.get((choice, comp_choice), 0)
+            self.send(f"Computer chose {comp_choice.upper()}, you {'win' if outcome.get((choice, comp_choice), 0) > 0 else 'lose' if outcome.get((choice, comp_choice), 0) < 0 else 'draw'}! You now have: {self.secret_value.value} points")
         elif choice == "k":
             self.send("You might have won, you might have lost. Who knows.")
         else:
@@ -147,13 +102,8 @@ New version of Rock Paper Scissors that includes a new move! Doesn't handle inte
             except ValueError:
                 self.send("Invalid Input.")
 
-# Handle multiple clients
-while True:
-    client_socket, client_address = server_socket.accept()
-    print(f"Connection from {client_address}")
-
+def handle_client(client_socket):
     challenge = CTFChallenge(client_socket)
-
     try:
         while True:
             choice = challenge.menu()
@@ -176,3 +126,9 @@ while True:
     except (ValueError, ConnectionResetError):
         challenge.send("Connection closed.")
         client_socket.close()
+
+while True:
+    client_socket, client_address = server_socket.accept()
+    print(f"Connection from {client_address}")
+    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+    client_thread.start()
